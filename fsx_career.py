131b3f446014e482680087b78a0a6c1d15d9b3f2
mlb406 <mlb406@gmail.com>
2016-03-30 16:35:49 +0100

    updated fsx_career.py

--- fsx_career.py
+++ fsx_career.py
@@ -5,3 +5,13 @@ import json
 def open_file(filename):
 	f = open(filename, "r");
 	return f
+
+
+
+def main():
+	airports = json.load(open_file("data/airports.json").read())
+	
+
+
+if __name__ == '__main__':
+	main()
